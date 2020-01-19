bfde3c8ee9f83113dfb2ccf555313a8f63c6df7e
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2020-01-19 08:13:13 -0800

    Preps some components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+import Grid from "./components/Grid";
 
 function App() {
-  return <div className="App">Hello no bug</div>;
+  return (
+    <div className="App">
+      <Grid />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Grid.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Grid(props) {
+    return (
+        <div>
+            grid works
+        </div>
+    )
+}
